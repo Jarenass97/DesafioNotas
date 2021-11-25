58248d1d8deeae010613e4f6d48897bacf1cf481
Jorge Arenas <jorgearenass97@gmail.com>
2021-11-25 19:02:26 +0100

    cambiado color de fondo

--- DesafioNotas/app/src/main/java/com/example/desafionotas/SplashScreen.kt
+++ DesafioNotas/app/src/main/java/com/example/desafionotas/SplashScreen.kt
@@ -7,7 +7,7 @@ import android.os.Bundle
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        
         var intentVS = Intent(this,MainActivity::class.java)
         startActivity(intentVS)
         finish()
